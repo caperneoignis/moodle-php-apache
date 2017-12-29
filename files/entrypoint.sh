3278ddc47fa1c325ee90b90a7a3d9c060392ac0f
Lee Kirkland <weeman.kirkland@gmail.com>
2017-12-28 21:04:55 -0500

    figured out the issue with constant crash
    extra '/' was being added which was throwing errors.

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -4,11 +4,13 @@ umask 022
 
 
 if [[ "${APACHE_WEB_ROOT}" != "" ]]; then
-    sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_WEB_ROOT}/#" /etc/apache2/sites-enabled/000-default.conf;
+    echo "we hit here"
+    sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_WEB_ROOT}/#" /etc/apache2/sites-enabled/000-default.conf
 else
+    echo "we hit else"
     #set a default if one is not present
-    APACHE_WEB_ROOT="/var/www/html/";
-    sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_WEB_ROOT}/#" /etc/apache2/sites-enabled/000-default.conf;
+    APACHE_ROOT="/var/www/html"
+    sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_ROOT}/#" /etc/apache2/sites-enabled/000-default.conf
 fi
 
 /usr/bin/supervisord -c /etc/supervisord.conf
