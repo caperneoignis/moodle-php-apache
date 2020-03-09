9c3e4abd177a4392f0bfaa67b6b43f7c61384b5e
Lee Kirkland <weeman.kirkland@gmail.com>
2020-03-09 12:25:49 -0400

    seeing if this fixes error.

--- root/tmp/setup/php-extensions.sh
+++ root/tmp/setup/php-extensions.sh
@@ -40,9 +40,9 @@ pecl install -o -f redis
 pecl install solr memcached apcu igbinary ${XDEBUG}
 
 # Need to reset xdebug from version to just xdebug.
-if [[ "${TAG}" == "7.0_pandoc_xdebug"]]
+if [[ "${TAG}" == "7.0_pandoc_xdebug" ]]
 then
-    XDEBUG = "xdebug"
+    XDEBUG="xdebug";
 fi
 
 docker-php-ext-enable solr memcached redis apcu igbinary ${XDEBUG}
